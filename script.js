f6b477bc91b9fbdbcca958baf28d382de533c4fc
diogenespasqualoto <diogenespasqualoto147@gmail.com>
2022-06-26 18:08:48 -0300

    Add Styles

--- script.js
+++ script.js
@@ -89,8 +89,6 @@ function loadItens() {
   })
 
 }
-
 const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? []
 const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens))
-
 loadItens()
